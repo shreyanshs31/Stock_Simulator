4a7598b22a337db9c38e47b487e3988b1219bc4e
Shreyansh Shukla <95318208+shreyanshs31@users.noreply.github.com>
2024-06-30 10:25:16 +0530

    added stock_list updated user.css file

---
+++ js/user.js
@@ -0,0 +1,32 @@
+const searchInput = document.querySelector("#input-el");
+const searchContainer = document.querySelector(".suggesstions-wrap");
+const suggesstionsContainer = document.querySelector(".suggesstions");
+
+
+function showRecommendedList(recommendedList) {
+
+    if (!recommendedList.length) {
+        return searchContainer.classList.remove("show");
+    }
+    searchContainer.classList.add("show");
+    suggesstionsContainer.innerHTML = "";
+    recommendedList.map(recommendedItem => {
+        const li = document.createElement("li");
+        li.textContent = recommendedItem.search;
+        suggesstionsContainer.appendChild(li);
+    })
+}
+
+
+async function filterSuggesstions() {
+    const response = await fetch("../stock_list.json")
+    const suggesstionsList = await response.json();
+    let searchValue = searchInput.value;
+    let recommendedList = [];
+    if (searchValue.length) {
+        recommendedList = suggesstionsList.filter(listItem => listItem.search.toLowerCase().includes(searchValue.toLowerCase()));
+    }
+    showRecommendedList(recommendedList);
+}
+
+searchInput.addEventListener("keyup", filterSuggesstions);
